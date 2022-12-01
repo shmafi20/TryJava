a7840b45770cb0f6fddd600ff4ee1522cc266161
Mafi Shahriyar <sh.mafi@SFAT.local>
2022-12-01 13:59:49 +0330

    next level

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,7 +1,7 @@
 package app;
 public class App{
     public static void main(String[] args) {
-        System.out.println("hello word!!!!!!!!");
+        System.out.println("hello word!??");
     }
 
 }
